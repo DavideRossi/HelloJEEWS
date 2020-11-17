31baa556509274087bf9b71243540f084d867059
Davide Rossi <daviderossi@unibo.it>
2020-11-17 15:40:02 +0100

    Initial commit

---
+++ src/main/java/it/unibo/soseng/WS.java
@@ -0,0 +1,13 @@
+package it.unibo.soseng;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class WS {
+
+    @WebMethod
+    public String Hello(String name) {
+        return "Hello "+name;
+    }
+}
